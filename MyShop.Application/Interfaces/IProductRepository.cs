07c76d1a6f37a79e4b873c13bc6e6f99c70148cb
togunchan <togunchan@gmail.com>
2025-07-07 22:27:14 +0300

    feat(application): define IProductRepository interface for product persistence

---
+++ MyShop.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyShop.Domain.Entities;
+
+namespace MyShop.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// Adds a new product to the store.
+        /// </summary>
+        /// <param name="product">The product entity to add.</param>
+        /// <returns>The Guid of the created product.</returns>
+        Task<Guid> AddAsync(Product product);
+
+        /// <summary>
+        /// Retrieves a product by its unique identifier.
+        /// </summary>
+        /// <param name="id">The Guid of the product.</param>
+        /// <returns>The product entity, or null if not found.</returns>
+        Task<Product?> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Lists all products in the store.
+        /// </summary>
+        /// <returns>A collection of product entities.</returns>
+        Task<IEnumerable<Product>> ListAllAsync();
+    }
+}
